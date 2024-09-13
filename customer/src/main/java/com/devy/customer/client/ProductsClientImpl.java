1bcd350511eb01085c68311e930aabdc6ddf44a4
Ilia Chuvikin <iliaivanovskiy@mail.ru>
2024-09-13 19:32:57 +0300

    Исправил ошибку: теперь страница продукта работает корректно

--- customer/src/main/java/com/devy/customer/client/ProductsClientImpl.java
+++ customer/src/main/java/com/devy/customer/client/ProductsClientImpl.java
@@ -2,11 +2,13 @@ package com.devy.customer.client;
 
 import com.devy.customer.model.Product;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
+@Slf4j
 public class ProductsClientImpl implements ProductsClient {
 
     private final WebClient webClient;
@@ -27,5 +29,6 @@ public class ProductsClientImpl implements ProductsClient {
                 .uri("/shop-api/products/{productId}", id)
                 .retrieve()
                 .bodyToMono(Product.class);
+
     }
 }
--- customer/src/main/java/com/devy/customer/controller/ProductController.java
+++ customer/src/main/java/com/devy/customer/controller/ProductController.java
@@ -1,12 +1,11 @@
 package com.devy.customer.controller;
 
 import com.devy.customer.client.ProductsClient;
-import com.devy.customer.model.Product;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import reactor.core.publisher.Mono;
@@ -14,17 +13,16 @@ import reactor.core.publisher.Mono;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("shop/customer/products/{productId:\\d+}")
+@Slf4j
 public class ProductController {
 
     private final ProductsClient productsClient;
 
-    @ModelAttribute(name = "product", binding = false)
-    public Mono<Product> loadProduct(@PathVariable("productId") Integer productId) {
-        return this.productsClient.findProduct(productId);
-    }
-
     @GetMapping
-    public String getProductPage(@ModelAttribute("product") Product product) {
-        return "shop/customer/products/product";
+    public Mono<String> getProductPage(@PathVariable("productId") Integer productId,
+                                 Model model) {
+        return productsClient.findProduct(productId)
+                .doOnNext(product -> model.addAttribute("product", product))
+                .then(Mono.just("shop/customer/products/product"));
     }
 }
