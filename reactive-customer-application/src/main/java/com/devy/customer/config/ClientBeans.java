53d54f3d8c362c327537c38c7495961b7f6cc23b
Ilia Chuvikin <iliaivanovskiy@mail.ru>
2024-09-21 09:32:19 +0300

    Добавил keycloak в реактивное приложение

--- reactive-customer-application/src/main/java/com/devy/customer/config/ClientBeans.java
+++ reactive-customer-application/src/main/java/com/devy/customer/config/ClientBeans.java
@@ -6,34 +6,60 @@ import com.devy.customer.client.ProductsClientImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ClientBeans {
 
+
+    // Scope("prototype") отвечает за то, что каждый раз, когда этот компонент будет внедряться в новый метод,
+    //будет создаваться новый экземпляр.
+    @Bean
+    @Scope("prototype")
+    public WebClient.Builder secondprojectServicesWebClientBuilder(
+            ReactiveClientRegistrationRepository clientRegistrationRepository,
+            ServerOAuth2AuthorizedClientRepository authorizedClientRepository
+    ) {
+        ServerOAuth2AuthorizedClientExchangeFilterFunction filter =
+                new ServerOAuth2AuthorizedClientExchangeFilterFunction(
+                        clientRegistrationRepository,
+                        authorizedClientRepository
+                );
+        filter.setDefaultClientRegistrationId("keycloak");
+        return WebClient.builder()
+                .filter(filter);
+    }
+
     @Bean
     public ProductsClientImpl productsClientImpl(
-            @Value("${shop.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUrl
+            @Value("${shop.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUrl,
+            WebClient.Builder secondprojectServicesWebClientBuilder
     ) {
-        return new ProductsClientImpl(WebClient.builder()
+        return new ProductsClientImpl(secondprojectServicesWebClientBuilder
                 .baseUrl(catalogueBaseUrl)
                 .build());
     }
 
     @Bean
     public FavoriteProductsClientImpl favoriteProductsClientImpl(
-            @Value("${shop.services.feedback.uri:http://localhost:8085}") String feedbackBaseUrl
+            @Value("${shop.services.feedback.uri:http://localhost:8085}") String feedbackBaseUrl,
+            WebClient.Builder secondprojectServicesWebClientBuilder
     ) {
-        return new FavoriteProductsClientImpl(WebClient.builder()
+        return new FavoriteProductsClientImpl(secondprojectServicesWebClientBuilder
                 .baseUrl(feedbackBaseUrl)
                 .build());
     }
 
     @Bean
     public ProductReviewsClientImpl productReviewsClientImpl(
-            @Value("${shop.services.feedback.uri:http://localhost:8085}") String feedbackBaseUrl
+            @Value("${shop.services.feedback.uri:http://localhost:8085}") String feedbackBaseUrl,
+            WebClient.Builder secondprojectServicesWebClientBuilder
     ) {
-        return new ProductReviewsClientImpl(WebClient.builder()
+        return new ProductReviewsClientImpl(secondprojectServicesWebClientBuilder
                 .baseUrl(feedbackBaseUrl)
                 .build());
     }
